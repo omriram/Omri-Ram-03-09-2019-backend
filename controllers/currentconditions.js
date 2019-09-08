ed16522d6575f88ce464f2f82f346f819e0b73a0
Omri Ram <omriram08@gmail.com>
2019-09-08 17:19:13 +0300

    removing testing comments

--- controllers/currentconditions.js
+++ controllers/currentconditions.js
@@ -3,34 +3,6 @@ const fetch = require("node-fetch");
 const handleCurrentConditionsApi = (req, res, apikey) => {
   const locationKey = req.body.locationKey;
 
-  /* const data = [
-    {
-      LocalObservationDateTime: "2019-09-07T16:41:00+03:00",
-      EpochTime: 1567863660,
-      WeatherText: "Clouds and sun",
-      WeatherIcon: 4,
-      HasPrecipitation: false,
-      PrecipitationType: null,
-      IsDayTime: true,
-      Temperature: {
-        Metric: {
-          Value: 28,
-          Unit: "C",
-          UnitType: 17
-        },
-        Imperial: {
-          Value: 82,
-          Unit: "F",
-          UnitType: 18
-        }
-      },
-      MobileLink:
-        "http://m.accuweather.com/en/gr/logos/2285860/current-weather/2285860?lang=en-us",
-      Link:
-        "http://www.accuweather.com/en/gr/logos/2285860/current-weather/2285860?lang=en-us"
-    }
-  ]; */
-
   fetch(
     "http://dataservice.accuweather.com/currentconditions/v1/" +
       locationKey +
